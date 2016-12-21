4a2bd84d1c598f3e8a6f0946189ae25669801da7
Andrew Voitov <andreivoitau@gmail.com>
2016-12-21 14:39:01 +0300

    Git ignore and fixes

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', config.port || 1313);
+app.set('port', 1313);
 
 app.use(express.static('frontend'));
 
 app.use('/api', bodyParser.json());
 
-logger.info('Error handler is initialized!');
-
 app.listen(app.get('port'), function () {
-    ('Hatebook is started on port:' + app.get('port'));
+    console.log('Hatebook is started on port:' + app.get('port'));
 });
