1f3104bf4e87ecd06852119972232f48e508799d
user-ПК\user <g1t1@mifort.org>
2016-12-22 02:02:13 +0300

    user service

--- frontend/services/user-service.js
+++ frontend/services/user-service.js
@@ -2,6 +2,9 @@ hateBook.factory('userService',['$http', function ($http) {
     return {
        getEnemies: function(){
            return $http.get('api/enemies');
-       }
+       },
+        getUser: function() {
+           return $http.get('api/current-user');
+        }
     };
 }]);
--- frontend/user/user.js
+++ frontend/user/user.js
@@ -1,5 +1,10 @@
 'use strict';
-hateBook.controller('userCtrl', ['$scope', '$rootScope',  function($scope, $rootScope) {
+hateBook.controller('userCtrl', ['$scope', '$rootScope','userService',  function($scope, $rootScope,userService) {
     /*$('.dropdown-toggle').dropdown()*/
     console.log('user');
+    userService.getUser().then(function(data) {
+        console.log(data.data);
+        
+    })
+
 }]);
