68662c4078166c4c524b4ba462f965eb3d986e1e
MaxStankevich <makstan1319@gmail.com>
2016-12-21 15:34:59 +0300

    get user API

--- app.js
+++ app.js
@@ -12,15 +12,28 @@ app.use(express.static('frontend'));
 
 app.use('/api', bodyParser.json());
 
-app.listen(app.get('port'), function () {
-    console.log('Hatebook is started on port:' + app.get('port'));
-});
-
 app.post('/api/signup', function (req, res) {
     db.userCollection().insert({email: req.body.email, password: req.body.password}, function(err, result) {
         if(!err){
             console.log("user successfully registered");
             res.send(result);
+        } else {
+            console.log(err);
         }
     })
+});
+
+app.get('/api/user/:id', function (req, res) {
+    db.userCollection().find({_id: ObjectId(req.params.id)}).toArray(function(err, docs) {
+        if(!err){
+            console.log("user successfully received");
+            res.send(docs);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Hatebook is started on port:' + app.get('port'));
 });
