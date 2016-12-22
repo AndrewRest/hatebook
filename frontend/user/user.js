ef41e305fc8c1ebd02a1ebb2e629c6469dc61481
MaxStankevich <makstan1319@gmail.com>
2016-12-22 04:46:57 +0300

    user profile page

--- frontend/user/user.js
+++ frontend/user/user.js
@@ -2,10 +2,21 @@
 hateBook.controller('userCtrl', ['$scope', '$rootScope','userService','$window',  function($scope, $rootScope,userService, $window) {
     /*$('.dropdown-toggle').dropdown()*/
     console.log('user');
-    userService.getUser().then(function(data) {
-        console.log(data.data);
-        $scope.currentUser = data.data;
-    });
+    $scope.getCurrentUserInfo = function () {
+        userService.getUser().then(function(data) {
+            console.log(data.data);
+            $scope.currentUser = data.data;
+            $scope.getPosts();
+        });
+    };
+
+    $scope.getPosts = function () {
+        userService.getPosts($scope.currentUser._id).then(function(data) {
+            console.log(data.data);
+            $scope.userPosts = data.data;
+        });
+    };
+
     $scope.toEdit = function() {
         $window.location.href = '/#/edit';
         console.log('asdasfasd');
