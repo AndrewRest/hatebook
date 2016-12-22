3a64d4a9c4ffde2cabba7ac7f3e0dc4074f272cd
MaxStankevich <makstan1319@gmail.com>
2016-12-22 09:13:11 +0300

    get current user fix

--- frontend/user/user.js
+++ frontend/user/user.js
@@ -1,19 +1,13 @@
 'use strict';
 hateBook.controller('userCtrl', ['$scope', '$rootScope','userService', '$location',  function($scope, $rootScope,userService, $location) {
-    /*$('.dropdown-toggle').dropdown()*/
-    console.log('user');
-    userService.getUser().then(function(data) {
-        console.log(data.data);
-    }, function(err){
-        $window.location.href='/#/login';
-        console.log(err);
-        }
-    );
     $scope.getCurrentUserInfo = function () {
         userService.getUser().then(function(data) {
             console.log(data.data);
             $scope.currentUser = data.data;
             $scope.getPosts();
+        }, function(err){
+            $location.path('/login');
+            console.log(err);
         });
     };
 
