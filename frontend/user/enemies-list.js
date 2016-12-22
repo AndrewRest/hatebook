a80205e41888d79be0e5c04c31791ee915d921e5
Tima4uk <timofey.chukleev@mifort.org>
2016-12-22 11:40:04 +0300

    add edit user info

--- frontend/user/enemies-list.js
+++ frontend/user/enemies-list.js
@@ -2,15 +2,15 @@ hateBook.controller('enemiesList',['$scope','userService','$location', function(
     $scope.enemies =[];
         userService.getEnemies().then(function(data){
             $scope.enemies = data.data;
-            console.log(data.data);
         },function(err){
-            $location.path('/login');
+            if(err.status == 401){
+                $location.path('/login');
+            }
             console.log(err);
         });
     $scope.getEnemies = function(id){
         $scope.addPoo = {userId:id};
         userService.userAddPoo($scope.addPoo).then(function(data){
-            console.log(data)
         }, function(err){
             console.log(err)
         });
